9f8ffd052a333263b54c5e2e83b71c8ecdbd04ba
theeasylearn <theeasylearn@gmail.com>
2026-03-03 10:58:26 +0530

    first commit

---
+++ 1_variable.py
@@ -0,0 +1,17 @@
+#create variable 
+name = "the easylearn academy"
+year = 2026
+pi = 3.14
+gender = True 
+#display variable 
+print(name)
+print(pi)
+print(gender)
+print(year)
+print("good morning ",name)
+print("current year",year)
+print("value of pi ",pi," gender =",gender)
+print(name,pi,gender,year)
+name = 100
+year = 'two thousand and twenty six'
+print(name,year)
