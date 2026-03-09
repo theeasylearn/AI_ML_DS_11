aaae0438b5c47bd3629131c864d3210ea013261e
theeasylearn <theeasylearn@gmail.com>
2026-03-09 11:31:20 +0530

    string and theory of list

---
+++ 7_string_1.py
@@ -0,0 +1,22 @@
+name = "the easylearn"
+print(name) #the easylearn 
+print(name[0]) #print 1st letter 't'
+print(name[1]) #h print 2nd letter h
+print(name[4]) #e
+
+#print 1st three letter
+print(name[0:3]) #the
+#print all letter from 4th position onwards
+print(name[4:]) #easylearn
+#print easy
+print(name[4:8])
+#print name variable's value 3 time
+print(name*3)
+city = "bhavnagar"
+print(name + city)
+pincode = 364001
+# print(name + pincode) #error because name is string and pincode is integer 
+# city[0] = "B" #error
+city = "baroda"
+print(city)
+
