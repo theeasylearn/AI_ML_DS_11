11e874e445235b1cb9872c3b91d5ad68ba0bd0fa
theeasylearn <theeasylearn@gmail.com>
2026-03-20 12:49:51 +0530

    decision making exercise

---
+++ 24_if_1.py
@@ -0,0 +1,9 @@
+#write a program to findout whether given number is positive number. if no is positive number then only display message number is positive number 
+number = int(input("Enter number"))
+print(number)
+# == != < > <= >=
+if number>0:
+    print("this number is positive number")
+    print("positive number are always greater then 0 ")
+
+print("good bye")
---
+++ 25_if_3.py
@@ -0,0 +1,3 @@
+#write a program to display name of day from given day 
+#day = 1 print monday
+#day = 2 print Tuesday
