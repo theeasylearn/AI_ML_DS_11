c0004176259ac4e571a5343d08b6c451c15a54b7
theeasylearn <theeasylearn@gmail.com>
2026-06-30 09:28:26 +0530

    3_project

--- database/3_project.py
+++ database/3_project.py
@@ -32,7 +32,25 @@ def AddBillItem():
 
             print("Item inserted")
     key = input("Press any key to continue")
+def SearchItemInBill():
+    name = input("Enter Item(product) Name")
+    sql = "SELECT bi.id 'item_id',name,rate,bi.quantity 'quantity' FROM `bill_items` bi, product p where p.id = productid and billid=0 and name like %s order by bi.id"
+    #create cursor 
+    cursor = database.connect.cursor(dictionary=True)
 
+    #run sql command 
+    cursor.execute(sql, (f"%{name}%",))
+
+    #fetch all records 
+    table = cursor.fetchall()
+    if len(table) == 0:
+        print("No bill item found")
+    else:
+        print(f"{'item_id':<10} {'name':<60} {'rate':<10} {'quantity':<10} {'total':<12}")
+        print("_"*110)
+        for row in table:
+            print(f"{row['item_id']:<10} {row['name']:<60} {row['rate']:<10} {row['quantity']:<10} {row['quantity'] * row['rate']:<12}")
+        print("_"*110)
 def DisplayBillItem():
     sql = "SELECT bi.id 'item_id',name,rate,bi.quantity 'quantity' FROM `bill_items` bi, product p where p.id = productid and billid=0 order by bi.id"
 
@@ -49,9 +67,58 @@ def DisplayBillItem():
     else:
         print(f"{'item_id':<10} {'name':<60} {'rate':<10} {'quantity':<10} {'total':<12}")
         print("_"*110)
+        GrandTotal=0
+        ItemCount = 0
         for row in table:
             print(f"{row['item_id']:<10} {row['name']:<60} {row['rate']:<10} {row['quantity']:<10} {row['quantity'] * row['rate']:<12}")
+            ItemCount+=1
+            GrandTotal += (row['quantity'] * row['rate'])
+        print("_"*110)
+        #display total item and Grand Total
+        print(f"No Of Items = {ItemCount:<58} Grand Total {GrandTotal:12}")
     key = input("Press any key to continue")
+def DeleteItemFromBill():
+    DisplayBillItem()
+    itemid = int(input("Enter Bill Item id to delete item from bill"))
+    sql = "delete from bill_items where id = %s"
+    data = [itemid]
+
+    #create cursor 
+    cursor = database.connect.cursor()
+
+    #run sql command
+    cursor.execute(sql,data)
+
+    database.connect.commit()
+    print("Item Deleted successfully")
+def GenerateBill():
+    confirm = input("Are you sure? you want to generate bill (press y/Y)")
+    if confirm.lower() == 'y':
+        # Task 1 check is there any unbilled item / calculate total 
+        # Task 2 reduce stock of each and every product added into bill_items 
+        # Task 3 accept customer name, email, mobile 
+        # Task 4insert row into customer table
+        
+        #Task 1 
+        sql = "SELECT sum(rate * quantity) as total FROM `bill_items` where billid=0"
+        cursor = database.connect.cursor(dictionary=True)
+        cursor.execute(sql)
+        row = cursor.fetchone()
+        if row['total'] == 0:
+            print("No item found in bill")
+        else:
+            #Task 2 
+            sql = "select productid, quantity from bill_items where billid=0"
+            cursor.execute(sql)
+            table = cursor.fetchall()
+            for row in table:
+                quantity = row['quantity']
+                productid = row['productid']
+                sql = "update product set quantity = quantity - %s where id = %s"
+                data = [quantity,productid]
+                cursor.execute(sql,data)
+            #Task 3
+            
 while True:
     print("\nPress 1 for Product management")
     print("Press 2 for Bill management")
@@ -112,11 +179,11 @@ while True:
                     if bill_choice == 1:
                         AddBillItem()
                     elif bill_choice == 2:
-                        print("let us delete item from bill")
+                        DeleteItemFromBill()
                     elif bill_choice == 3:
-                        print("let us search item in bill")
+                        SearchItemInBill()
                     elif bill_choice == 4:
-                        print("let us generate bill")
+                        GenerateBill()
                     elif bill_choice == 5:
                         DisplayBillItem()
                     else:
